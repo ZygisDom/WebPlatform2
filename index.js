215add59986faf2f28466a5e68656fa9203755a1
Samuel Brown <sbrown228@caledonian.ac.uk>
2020-05-06 14:34:42 +0100

    Added Task handling and other small fixes/functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require ("express"),
+path = require("path"),
+mustache = require('mustache-express'),
+bodyParser = require('body-parser'),
+controller = require('./controllers/routes.js');
+
+var app = express();
+
+app.set('port', process.env.PORT || 9000);
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+//var staticPath = path.resolve(__dirname, "/static");
+app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', controller);
+
+app.use(function (req, res) {
+    res.status(404);
+    res.type('text/plain');
+    res.send("404 - Not found.");
+});
+
+app.listen(app.get('port'), function () {
+    console.log('server started, ctl^c to quit');
+})
+
